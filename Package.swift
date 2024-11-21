6dbb7b4736ff75752c7e4cd2c0acfa2dc221dba6
Kajal <38717258+Kajalluthra@users.noreply.github.com>
2024-11-21 19:53:18 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,9 @@ let package = Package(
             name: "LoggerExtension",
             targets: ["LoggerExtension"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Kajalluthra/Account.git", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
